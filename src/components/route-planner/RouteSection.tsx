6ee6e44976976fc847790a99a603817ba8ed1d01
chrischtili <christian@kopmann.com>
2026-02-04 17:28:58 +0000

    Fix date input height to match regular text inputs exactly
    - Changed height from h-10 to h-[42px] to match default Input component height
    - Added explicit height to wrapper div for consistent container sizing
    - Simplified WebKit styling to focus on essential centering
    - Date inputs should now be identical in size to Start/Ziel fields
    - Maintains all centering functionality

--- src/components/route-planner/RouteSection.tsx
+++ src/components/route-planner/RouteSection.tsx
@@ -42,28 +42,28 @@ export function RouteSection({ formData, onChange }: RouteSectionProps) {
 
         <div className="space-y-2">
           <Label htmlFor="startDate">Abreise</Label>
-          <div className="relative">
+          <div className="relative h-[42px]">
             <Input
               id="startDate"
               type="date"
               value={formData.startDate}
               min={new Date().toISOString().split('T')[0]}
               onChange={(e) => onChange({ startDate: e.target.value, endDate: e.target.value })}
-              className="w-full text-center h-10 [&::-webkit-datetime-edit]:text-center [&::-webkit-datetime-edit]:justify-center [&::-webkit-datetime-edit-year-field]:text-center [&::-webkit-datetime-edit-month-field]:text-center [&::-webkit-datetime-edit-day-field]:text-center [&::-webkit-datetime-edit-text]:text-center [&::-webkit-datetime-edit]:mx-auto [&::-webkit-datetime-edit]:w-auto"
+              className="w-full text-center h-[42px] [&::-webkit-datetime-edit]:text-center [&::-webkit-datetime-edit-year-field]:text-center [&::-webkit-datetime-edit-month-field]:text-center [&::-webkit-datetime-edit-day-field]:text-center [&::-webkit-datetime-edit-text]:text-center"
             />
           </div>
         </div>
 
         <div className="space-y-2">
           <Label htmlFor="endDate">Ankunft</Label>
-          <div className="relative">
+          <div className="relative h-[42px]">
             <Input
               id="endDate"
               type="date"
               value={formData.endDate}
               min={formData.startDate || new Date().toISOString().split('T')[0]}
               onChange={(e) => onChange({ endDate: e.target.value })}
-              className="w-full text-center h-10 [&::-webkit-datetime-edit]:text-center [&::-webkit-datetime-edit]:justify-center [&::-webkit-datetime-edit-year-field]:text-center [&::-webkit-datetime-edit-month-field]:text-center [&::-webkit-datetime-edit-day-field]:text-center [&::-webkit-datetime-edit-text]:text-center [&::-webkit-datetime-edit]:mx-auto [&::-webkit-datetime-edit]:w-auto"
+              className="w-full text-center h-[42px] [&::-webkit-datetime-edit]:text-center [&::-webkit-datetime-edit-year-field]:text-center [&::-webkit-datetime-edit-month-field]:text-center [&::-webkit-datetime-edit-day-field]:text-center [&::-webkit-datetime-edit-text]:text-center"
             />
           </div>
         </div>
